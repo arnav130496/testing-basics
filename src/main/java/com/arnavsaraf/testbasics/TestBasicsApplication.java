08a8da4d9219037495417a2dca10c7e3edaa7ebc
Arnav <arnav130496@gmail.com>
2021-09-16 16:43:00 +0530

    project init

---
+++ src/main/java/com/arnavsaraf/testbasics/TestBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.arnavsaraf.testbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnavsaraf/testbasics/TestBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnavsaraf.testbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
